4870eae25f7b9d3efeeaed52f08dec9ef471d4d0
Mariana Dias <marianamkt6@gmail.com>
2024-11-05 11:02:59 -0300

    projeto finalizado

--- form/src/components/form.tsx
+++ form/src/components/form.tsx
@@ -1,5 +1,6 @@
 import {User} from '../types/User'
 import { useState, FormEvent } from 'react'
+import { validate } from '../utils/validate';
 
 
 const Form = () =>{
@@ -9,8 +10,22 @@ const Form = () =>{
      const [agree, setAgree] = useState(false);
      const [erros,setErros] = useState<User | null>(null);
 
-     const handleSubmit = (e: FormEvent) =>{
+     const handleSubmit = (e: FormEvent) =>{ //tipei o 'e' pois se não tipar, seria retornado um 'any'
         e.preventDefault();
+        setErros(null);
+    
+        const data:User = {
+            name,email,agree
+        };
+     const validateErros = validate(data);
+     
+     if(Object.keys(validateErros).length > 0){
+       setErros(validateErros)
+     }
+     
+     setName("");
+     setEmail("");
+     setAgree(false);
  
      }
 
@@ -18,18 +33,22 @@ const Form = () =>{
         <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="flex flex-col">
                 <label className="text-sm" htmlFor="name">Nome</label>
-                <input type="text" placeholder="Digite seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"></input>
+                <input type="text" placeholder="Digite seu nome" className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400" value={name} onChange={(e)=> setName(e.target.value)}></input>
             </div>
+            {erros?.name && <small className='text-xs text-red-500 mt-1'>{erros?.name}</small>}
             <div  className="flex flex-col">
                 <label  className="text-sm" htmlFor="email">Email</label>
-                <input type="email" placeholder="Insira seu melhor e-mail"  className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"></input>
+                <input type="email" placeholder="Insira seu melhor e-mail"  className="rounded-lg py-2 px-2 text-sm placeholder:text-sm placeholder:text-stone-400"value={email} onChange={(e)=> setEmail(e.target.value)}></input>
             </div>
+            {erros?.email && <small className='text-xs text-red-500 mt-1'>{erros?.email}</small>}
             <div>
                 <a href="#" className="text-xs underline mb-2">Leia os termos</a>
                 <div  className="flex gap-2 items-center">
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={agree} onChange={(e)=> setAgree(e.target.checked)}/>
                     <label  className="text-sm"  htmlFor="agree">Concordo com os termos</label>
+                  
                 </div>
+                {erros?.agree && <small className='text-xs text-red-500 mt-1'>{erros?.agree}</small>}
             </div>
            <button type="submit" className="bg-slate-600 hover:bg-slate-500 font-medium text-sm py-2 px-4 rounded-lg text-white">Cadastrar</button>
         </form>
